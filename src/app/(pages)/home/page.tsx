bfcafe555d7e96d99e900012d7f96ec7aa92fce2
Akinola Femi <akinolafemi573@gmail.com>
2025-07-08 21:41:09 +0100

    fix stats import components error

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,8 +1,8 @@
 import HeroAnimation from '@/app/components/UI/HeroAnimation';
-import About from '@/app/components/UI/About';
-import Stats from '@/app/components/UI/Stats';
-import VisaCard from '@/app/components/UI/VisaCard';
-import WhyChooseUs from '@/app/components/UI/WhyChooseUs';
+import About from '../../components/UI/About';
+import Stats from '../../components/UI/Stats';
+import VisaCard from '../../components/UI/VisaCard';
+import WhyChooseUs from '../../components/UI/WhyChooseUs';
 // import Testimonials from '@/app/components/UI/Testimonials';
 // import '../../globals.css'
 
--- src/app/Components/UI/WhyChooseUs.tsx
+++ src/app/Components/UI/WhyChooseUs.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const WhyChooseUs = () => {
   return (
     <div>
-      
+      <h1></h1>
     </div>
   )
 }
